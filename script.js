9e2c16469c2a7873089d0fabbe1bb4c1a1725129
Koustav Mohapatra <go2amul@gmail.com>
2025-01-12 22:13:58 +0530

    changes in index and syles

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ ScrollReveal().reveal('.home-content,.heading,.about-content p', { origin: 'top'
 ScrollReveal().reveal('.home-img,.services-container, .portfolio-box, .contact form, .gallery-container' ,{ origin: 'bottom ' });
 
 const typed = new Typed('.multi',{
-    strings: ['Frontend Developer', 'Tablist', 'Core Developer', 'Student'],
+    strings: ['Web Dev Intern', 'Tablist', 'Student'],
 
     typeSpeed:100,
     backSpeed:100,
